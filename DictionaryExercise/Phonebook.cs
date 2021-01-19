6b6cd396bb96fca334d7eaf5399c63aa075b2ac1
ZestyR0b0t <40412951+ZestyR0b0t@users.noreply.github.com>
2021-01-19 15:42:15 -0500

    Dictionary exercise complete

--- DictionaryExercise/Phonebook.cs
+++ DictionaryExercise/Phonebook.cs
@@ -5,29 +5,36 @@ namespace DictionaryExercise
 {
     public class Phonebook
     {
+        private readonly Dictionary<string, string> firstDictionary = new Dictionary<string, string>();
         private const string PHONEBOOK_FILE = "phonebook.txt";
         
         public Phonebook()
         {
-            // Empty contstructor, you can remove this if you don't want to use it.
+            InitPhonebookFromFile();
         }
 
-        // Initializes our phonebook from a file. This should only ever be called once. When do you think the best time to call this would be?
         private void InitPhonebookFromFile()
         {
             string[] phonebookLines = System.IO.File.ReadAllLines(PHONEBOOK_FILE); // Read in all lines from "phonebook" file
 
             foreach (string phonebookLine in phonebookLines)
             {
-                // Each line in the file is formatted like this:
-                // PERSON NAME | PHONE NUMBER
                 string[] nameAndNumber = phonebookLine.Split('|', StringSplitOptions.TrimEntries);
 
                 string name = nameAndNumber[0];
                 string phoneNumber = nameAndNumber[1];
 
-                // TODO: Add name and phone number to our phonebook
+                firstDictionary[name] = phoneNumber;
             }
+        }
+        public void LookUpNumberBasedOnName(string name)
+        {
+            if (firstDictionary.ContainsKey(name))
+            {
+                Console.WriteLine($"{firstDictionary[name]}");
+                return;
+            }
+            Console.WriteLine($"No contact by the name of {name} was found!");
         }
     }
 }
--- DictionaryExercise/Program.cs
+++ DictionaryExercise/Program.cs
@@ -7,7 +7,7 @@ namespace DictionaryExercise
         static void Main(string[] args)
         {
             Phonebook myContacts = new Phonebook();
-
+            
             while(true)
             {
                 Console.WriteLine("Type a name to search phonebook, or \"exit\" to quit.\n");
@@ -18,7 +18,12 @@ namespace DictionaryExercise
                     Environment.Exit(0);
                     return;
                 }
-
+                myContacts.LookUpNumberBasedOnName(input);
+                Console.WriteLine("-------------\n");
+            }
+        }
+    }
+}
                 // If we get here, assume input string is the name of a person.
 
                 // TODO: Try to find phone number for the specified person in our "phonebook".
@@ -26,9 +31,3 @@ namespace DictionaryExercise
                 // TODO: If we find a phone number for the specified person, print it to console.
 
                 // TODO: If we don't find a phone number for the specified person, write to console "No contact by the name of <what user typed> was found!"
-
-                Console.WriteLine("-------------\n");
-            }
-        }
-    }
-}
