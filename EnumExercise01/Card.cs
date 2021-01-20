ca24e260ada95afca30dec8bea58e125156462d5
Sarah Plotkin <splotkin@bighugegames.com>
2021-01-20 10:03:33 -0500

    [SEP] Add EnumExercise01.

---
+++ EnumExercise01/Card.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace EnumExercise01
+{
+    public class Card
+    {
+        // TODO: Fill out this class!
+
+        // TODO: Add enums for rank and suit! You can define enums right in this class, if you want them to be "paired" (associated) with the class itself.
+        //       When you define an enum within another class, you need to qualify the name of the enum with the outer class name. So if I define an "Action" class,
+        //       and I define a "Type" enum within that class, if I want to refer to that enum outside of the "Action" class, I would need to refer to it as "Action.Type".
+
+        // Valid Ranks: Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace
+        // Valid Suits: Clubs, Diamonds, Hearts, Spades
+    }
+}
---
+++ EnumExercise01/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace EnumExercise01
+{
+    class Program
+    {
+        // DO NOT TOUCH THIS METHOD
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("** Type a card name. Ex: \"queen of hearts\" **");
+                string input = Console.ReadLine().ToLowerInvariant();
+
+                string[] inputs = input.Split(" of ", 2, StringSplitOptions.TrimEntries);
+
+                if (inputs.Length < 2)
+                {
+                    if (inputs.Length == 1 && inputs[0] == "quit")
+                    {
+                        Environment.Exit(0);
+                        return;
+                    }
+
+                    Console.WriteLine("** Not enough parameters. Please try again. **\n\n");
+                    continue;
+                }
+
+                string cardRank = inputs[0];
+                string cardSuit = inputs[1];
+
+                ParseInput(cardRank, cardSuit);
+            }
+        }
+
+        static void ParseInput(string inputRank, string inputSuit)
+        {
+            // TODO: If rank is invalid, print "** X is not a valid rank **", where "X" is the invalid rank the user provided.
+            // TODO: If suit is invalid, print "** X is not a valid suit **", where "X" is the invalid suit the user provided.
+
+            // TODO: If the suit and rank are valid, create a "Card" object with that rank and suit, then uncomment the line below. Add the necessary "Get" methods to the card class.
+            //Console.WriteLine($"** Rank: {card.GetRank()} | Suit: {card.GetSuit()}**\n\n");
+
+            // IMPORTANT NOTES!!!! (READ THESE BEFORE WRITING ANY CODE)
+            // - You can pass an enum value directly into "Console.WriteLine" or a formatted string (the kind with ${}), and it will auto-convert to a string. No need to manually convert to string.
+            // - If you have a string, and you want to parse it into an enum value, you can use the method Enum.TryParse<EnumType>(input, ignoreCase, out result). The syntax is weird, so
+            //   here's a snippet link that shows how how to use it: https://dotnetfiddle.net/O4EhwA
+            //   I'll explain that weird syntax to you in-person at some point. Here's the official documentation if you'd like more info: https://docs.microsoft.com/en-us/dotnet/api/system.enum.tryparse?view=net-5.0#System_Enum_TryParse__1_System_String_System_Boolean___0__
+        }
+    }
+}
