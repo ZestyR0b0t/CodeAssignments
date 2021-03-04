b6708687bc745e065aa8f7e6e3a67c3f691f3956
ZestyR0b0t <40412951+ZestyR0b0t@users.noreply.github.com>
2021-03-04 08:54:08 -0500

    Now it's done?

--- Linked List Exercise/Linked List Exercise/Program.cs
+++ Linked List Exercise/Linked List Exercise/Program.cs
@@ -8,19 +8,7 @@ namespace Linked_List_Exercise
     {
         static void Main(string[] args)
         {
-            List<int> thisList = new List<int>();
-            thisList.Add(1);
-            thisList.Add(2);
-            thisList.Add(3);
-            thisList.Add(4);
-
-            thisList.RemoveAt(0);
-
-            thisList.ForEach(Console.WriteLine);
-
-            Console.WriteLine(thisList[0]);
-
-            Console.ReadLine();
+           
         }
     }
 }
