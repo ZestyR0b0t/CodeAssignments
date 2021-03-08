3ad8fffc4aa7590b8c9c287be347c5d452d2abd4
Sarah Plotkin <splotkin@bighugegames.com>
2021-03-08 10:05:40 -0500

    Adding new "DataStructures" assignment, with Stack/Queue class stubs.

---
+++ DataStructures/DataStructures/Queue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+// DO NOT USE System.Collections.Generic OR System.Collections IN HERE!!!
+
+namespace DataStructures
+{
+    class Queue
+    {
+        // Gets how many elements are in the queue.
+        public int Count { get; private set; }
+
+        // Returns the item at the beginning of the queue without removing it.
+        public string Peek()
+        {
+            return null; // TODO: Implement this
+        }
+
+        // Removes and returns the item at the beginning of the queue.
+        public string Dequeue()
+        {
+            return null; // TODO: Implement this
+        }
+
+        // 	Adds an item to the end of the queue.
+        public void Enqueue(string item)
+        {
+            // TODO: Implement this
+        }
+
+        // Prints out all items in the queue, as a comma-separated list. The
+        // output should also include the words "[FRONT]" and "[BACK]" and the beginning/end (or vice-versa)
+        // to indicate the front and back of your queue. Which one comes first depends on how you set up
+        // your queue.
+        // (ex. where 4 was the last item enqueued: [FRONT] 1, 2, 3, 4 [BACK])
+        public void PrintAll()
+        {
+            // TODO: Implement this
+        }
+    }
+}
---
+++ DataStructures/DataStructures/Stack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+// DO NOT USE System.Collections.Generic OR System.Collections IN HERE!!!
+
+namespace DataStructures
+{
+    // Represents a simple last-in-first-out (LIFO) collection of items.
+    class Stack
+    {
+        // Gets how many elements are on the stack.
+        public int Count { get; private set; }
+
+        // Returns the item on the top of the stack without removing it.
+        public string Peek()
+        {
+            return null; // TODO: Implement this
+        }
+
+        // Removes and returns the object at the top of the stack.
+        public string Pop()
+        {
+            return null; // TODO: Implement this
+        }
+
+        // Inserts the given item at the top of the Stack.
+        public void Push(string item)
+        {
+            // TODO: Implement this
+        }
+
+        // Prints out all items in the stack, as a comma-separated list. The
+        // output should also include the words "[TOP]" and "[BOTTOM]" and the beginning/end (or vice-versa)
+        // to indicate the top and bottom of your stack. Which one comes first depends on how you set up
+        // your stack.
+        // (ex. where 4 was the last item pushed onto the stack: [BOTTOM] 1, 2, 3, 4 [TOP])
+        public void PrintAll()
+        {
+            // TODO: Implement this
+        }
+    }
+}
